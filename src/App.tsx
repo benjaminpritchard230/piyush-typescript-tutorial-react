54126796586ba05b82ba752f516058ce2383ee9f
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-01-02 04:23:59 +0000

    Learn typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,29 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import InputField from "./components/InputField";
-
+import { useState } from "react";
+import { Todo } from "./model";
+import { light } from "@mui/material/styles/createPalette";
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Math.random(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
   return (
     <div className="App">
       <span className="heading">Taskify </span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map((todo) => (
+        <li>{todo.todo}</li>
+      ))}
     </div>
   );
 };
